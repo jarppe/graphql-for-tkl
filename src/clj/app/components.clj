3803f30838d1b319ac1dbbd86f7653705d5057e8
Jarppe <jarppe@metosin.fi>
2018-04-04 17:52:00 +0300

    Refactor graphql handler to be just plain fn, use default components config as a base.

--- src/clj/app/components.clj
+++ src/clj/app/components.clj
@@ -1,15 +1,8 @@
 (ns app.components
-  (:require [integrant.core :as ig]
+  (:require [syksy.core :as syksy]
             [syksy.web.index :as index]
             [app.gql.handler :as graphql]))
 
 (defn components []
-  {:syksy.web.server/server {:handlers [(ig/ref :syksy.web.api/handler)
-                                        (ig/ref :syksy.web.index/handler)
-                                        (ig/ref :syksy.web.resources/handler)
-                                        (ig/ref :syksy.web.not-found/handler)]}
-   :syksy.web.api/handler {:routes (ig/ref ::graphql/handler)}
-   :syksy.web.index/handler {:index-body (index/index {:title "GraphQL API for TKL"})}
-   :syksy.web.resources/handler {}
-   :syksy.web.not-found/handler {}
-   ::graphql/handler {}})
+  (syksy/default-components {:index-body (index/index {:title "GraphQL API for TKL"})
+                             :routes graphql/handler}))
--- src/clj/app/gql/handler.clj
+++ src/clj/app/gql/handler.clj
@@ -1,6 +1,5 @@
 (ns app.gql.handler
-  (:require [integrant.core :as ig]
-            [ring.util.http-response :as resp]
+  (:require [ring.util.http-response :as resp]
             [com.walmartlabs.lacinia :as gql]
             [schema.core :as s]
             [reitit.core :as r]
@@ -12,28 +11,44 @@
 (s/defschema GraphQLResponse {(s/optional-key :data) s/Any
                               (s/optional-key :errors) [s/Any]})
 
-(defn handler [req]
+(defn gql-get-handler [req]
   (gql/execute c/schema
                (-> req :parameters :query :query)
                nil
                nil))
 
-(defmethod ig/init-key ::handler [_ _]
+(defn gql-post-handler [req]
+  (gql/execute c/schema
+               (-> req :parameters :query :query)
+               nil
+               nil))
+
+(def handler
   (ring/ring-handler
     (ring/router
-      ["/graphql" {:get {:parameters {:query {:query s/Str}}
-                         :responses {200 {:body GraphQLResponse}}
-                         :handler (comp resp/ok handler)}}]
+      ["/graphql" {:name :graphql
+                   :get {:parameters {:query {:query s/Str}}
+                         :handler (comp resp/ok gql-get-handler)
+                         :responses {200 {:body GraphQLResponse}}}
+                   :post {:parameters {:body {:query s/Str}}
+                          :handler (comp resp/ok gql-post-handler)
+                          :responses {200 {:body GraphQLResponse}}}}]
       {:data {:coercion reitit.coercion.schema/coercion
               :middleware [rrc/coerce-exceptions-middleware
                            rrc/coerce-request-middleware
                            rrc/coerce-response-middleware]}})))
 
+(handler {:request-method :post
+          :uri "/graphql"
+          :body-params {:query "foo"}})
 
 (comment
 
   (require '[clj-http.client :as http])
   (http/get "http://localhost:3000/graphql" {:query-params {:query "{line(id: \"1A\") {name}}"}
-                                             :accept :json})
+                                             :accept :json
+                                             :as :json})
+
+  ; http :3000/graphql query=='{line(id: "1A") {name description}}'
 
   )
