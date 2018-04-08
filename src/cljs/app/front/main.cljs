6d980468d76e767b3c5c0edd9bdc7617ee0392e5
Jarppe <jarppe@metosin.fi>
2018-04-08 23:14:21 +0300

    Add some information to front.

--- src/cljs/app/front/main.cljs
+++ src/cljs/app/front/main.cljs
@@ -7,8 +7,40 @@
 ;; Main view:
 ;;
 
+(defn link-row [name url]
+  [:tr
+   [:td name]
+   [:td [:a {:href url :target "_blank"} url]]])
+
 (defn main-view []
-  [:h1 "Here we go again..."])
+  [:div
+   [:h1 "GraphQL API for TKL Journey API"]
+   [:div.info
+    [:div "This is a GraphQL test project. I wanted to learn GraphQL and
+           decided to use the Journey API as a data source. Journey API
+           provides a RESTful API for public transport information in
+           Tampere area."]
+    [:div "This project is still under development and currently the
+           API supports only a very limited subset of Journey API
+           functionality."]
+    [:div "You can test the GraphQL API with the interactive "
+     "Graph" [:i "i"] "QL client "
+     [:a {:href "/graphiql"} "here"]
+     "."]]
+   [:div.links
+    "Here's some related links:"
+    [:table
+     [:tbody
+      [link-row "GraphQL" "https://graphql.org/"]
+      [link-row "GraphiQL" "https://github.com/graphql/graphiql/"]
+      [link-row "Clojure" "https://clojure.org/"]
+      [link-row "Journey API" "http://wiki.itsfactory.fi/index.php/Journeys_API"]
+      [link-row "TKL" "http://joukkoliikenne.tampere.fi/"]
+      [link-row "This project" "https://github.com/jarppe/graphql-for-tkl/"]]]]
+   [:div.info
+    [:div "This service is running on Heroku free instance. Sometimes
+           the service is sleeping, please allow some time for wakeup."]]
+   [:div.copy "Copyright © 2018 " [:a {:href "https://github.com/jarppe/"} "Jarppe Länsiö"]]])
 
 ;;
 ;; Main
