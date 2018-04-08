851c9a9d175afef2895efafbeb9e15203f36ecdc
Jarppe <jarppe@metosin.fi>
2018-04-08 20:21:00 +0300

    Update deps, cleanup unused deps, move prod profile stuff under uberjar.

--- project.clj
+++ project.clj
@@ -1,18 +1,17 @@
 (defproject jarppe/graphql-for-tkl "0.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
 
-                 ; Syksy
-                 [jarppe/syksy "0.0.0-SNAPSHOT"]
+                 ; Syksy:
+                 [jarppe/syksy "0.0.1"]
 
                  ; Common stuff:
                  [org.clojure/core.async "0.4.474"]
                  [prismatic/schema "1.1.9"]
                  [potemkin "0.4.5"]
+                 [metosin/potpuri "0.5.1"]
 
                  ; GraphQL:
                  [com.walmartlabs/lacinia "0.25.0"]
-                 [vincit/venia "0.2.5"]
-                 [locksmith "0.1.0-SNAPSHOT"]
 
                  [metosin/reitit "0.1.1-SNAPSHOT"]
                  [metosin/reitit-ring "0.1.1-SNAPSHOT"]
@@ -23,10 +22,10 @@
 
                  ; ClojureScript:
                  [org.clojure/clojurescript "1.10.238"]
-                 [binaryage/devtools "0.9.9"]
+                 [binaryage/devtools "0.9.10"]
                  [metosin/reagent-dev-tools "0.2.0"]
                  [reagent "0.8.0-alpha2"]
-                 [cljs-http "0.1.44"]]
+                 [cljs-http "0.1.45"]]
 
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/clj"]
@@ -44,12 +43,13 @@
             [lein-figwheel "0.5.15"]
             [lein-cljsbuild "1.1.7"]]
 
-  :profiles {:dev {:resource-paths ["target/dev/resources"]
+  :profiles {:dev {:dependencies [[metosin/testit "0.2.1"]]
+                   :resource-paths ["target/dev/resources"]
                    :sass {:target-path "target/dev/resources/public/css"}}
-             :prod {:source-paths ^:replace ["src/clj" "src/cljs"]
-                    :resource-paths ["target/prod/resources"]
-                    :sass {:target-path "target/prod/resources/public/css"}}
-             :uberjar {:main syksy.main
+             :uberjar {:source-paths ^:replace ["src/clj" "src/cljs"]
+                       :resource-paths ["target/prod/resources"]
+                       :sass {:target-path "target/prod/resources/public/css"}
+                       :main syksy.main
                        :aot [syksy.main]
                        :uberjar-name "app.jar"}}
 
@@ -77,8 +77,8 @@
 
   :aliases {"dev" ["do" "clean"
                    ["pdo" ["sass4clj" "auto"] ["figwheel"]]]
-            "prod" ["with-profile" "prod" "do"
-                    "clean"
+            "prod" ["with-profile" "uberjar" "do"
+                    ["clean"]
                     ["sass4clj" "once"]
                     ["cljsbuild" "once" "prod"]
-                    "uberjar"]})
+                    ["uberjar"]]})
