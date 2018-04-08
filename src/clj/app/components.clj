4682c5fd34db844b6a1229fef8fa4e3a34c3915d
Jarppe <jarppe@metosin.fi>
2018-04-08 23:14:12 +0300

    Refactor how GraphiQL is served.

--- src/clj/app/components.clj
+++ src/clj/app/components.clj
@@ -1,26 +1,24 @@
 (ns app.components
   (:require [integrant.core :as ig]
-            [syksy.core :as syksy]
-            [syksy.web.resources :as resources]
-            [syksy.web.index :as index]
-            [syksy.web.redirect :as redirect]
-            [syksy.web.server :as server]
             [potpuri.core :as p]
+            [syksy.core :as syksy]
+            [syksy.web.index :as index]
+            [syksy.web.resource :as resource]
+            [syksy.web.resources :as resources]
             [app.gql.handler :as graphql]))
 
 (defn components []
-  (let [port (or (some-> (System/getenv "PORT")
-                         (Integer/parseInt))
-                 3000)]
-    (p/deep-merge
-      (syksy/default-components {:index-body (index/index {:title "GraphQL API for TKL Journey API"})
-                                 :routes graphql/handler
-                                 :addon-handlers [(ig/ref [::resources/handler ::app])
-                                                  (ig/ref [::redirect/handler ::app])]})
-      ; Serve "/graphiql/" resources under /resources/graphiql and
-      ; redirect "/graphiql" and "/graphiql/" -> "/graphiql/index.html"
-      {[::server/server ::syksy/syksy] {:port port, :host "0.0.0.0"}
-       [::resources/handler ::app] {:asset-prefix "/graphiql/"
-                                    :asset-dir "graphiql/"}
-       [::redirect/handler ::app] {:from (comp #{"/graphiql" "/graphiql/"} :uri)
-                                   :location "/graphiql/index.html"}})))
+  (p/deep-merge
+    (syksy/default-components {:host "0.0.0.0"
+                               :port (or (some-> (System/getenv "PORT")
+                                                 (Integer/parseInt))
+                                         3000)
+                               :index-body (index/index {:title "GraphQL API for TKL Journey API"})
+                               :routes graphql/handler
+                               :addon-handlers [(ig/ref [::resource/handler ::app])
+                                                (ig/ref [::resources/handler ::app])]})
+    {[::resource/handler ::app] {:match? {"/graphiql" "graphiql/index.html"
+                                          "/graphiql/" "graphiql/index.html"
+                                          "/favicon.ico" "public/favicon.ico"}}
+     [::resources/handler ::app] {:asset-prefix "/graphiql/"
+                                  :asset-dir "graphiql/"}}))
