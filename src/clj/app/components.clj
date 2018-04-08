bd978ca3abb04848976a393ee55b36ef245e9246
Jarppe <jarppe@metosin.fi>
2018-04-08 20:25:07 +0300

    Tune components, use HTTP port from env "PORT" or default to 3000.

--- src/clj/app/components.clj
+++ src/clj/app/components.clj
@@ -1,9 +1,23 @@
 (ns app.components
-  (:require [syksy.core :as syksy]
+  (:require [integrant.core :as ig]
+            [syksy.core :as syksy]
+            [syksy.web.resources :as resources]
             [syksy.web.index :as index]
+            [syksy.web.redirect :as redirect]
+            [syksy.web.server :as server]
+            [potpuri.core :as p]
             [app.gql.handler :as graphql]))
 
 (defn components []
-  (syksy/default-components
-    {:index-body (index/index {:title "GraphQL API for TKL Journey API"})
-     :routes graphql/handler}))
+  (p/deep-merge
+    (syksy/default-components {:index-body (index/index {:title "GraphQL API for TKL Journey API"})
+                               :routes graphql/handler
+                               :addon-handlers [(ig/ref [::resources/handler ::app])
+                                                (ig/ref [::redirect/handler ::app])]})
+    ; Serve "/graphiql/" resources under /resources/graphiql and
+    ; redirect "/graphiql" and "/graphiql/" -> "/graphiql/index.html"
+    {[::server/server ::syksy/syksy] {:port (or (System/getenv "PORT") 3000)}
+     [::resources/handler ::app] {:asset-prefix "/graphiql/"
+                                  :asset-dir "graphiql/"}
+     [::redirect/handler ::app] {:from (comp #{"/graphiql" "/graphiql/"} :uri)
+                                 :location "/graphiql/index.html"}}))
