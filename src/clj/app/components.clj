f81e4d081aac55060d68b963ee49a11eb378cbd3
Jarppe <jarppe@metosin.fi>
2018-04-08 20:43:01 +0300

    Parse port to int

--- src/clj/app/components.clj
+++ src/clj/app/components.clj
@@ -9,15 +9,18 @@
             [app.gql.handler :as graphql]))
 
 (defn components []
-  (p/deep-merge
-    (syksy/default-components {:index-body (index/index {:title "GraphQL API for TKL Journey API"})
-                               :routes graphql/handler
-                               :addon-handlers [(ig/ref [::resources/handler ::app])
-                                                (ig/ref [::redirect/handler ::app])]})
-    ; Serve "/graphiql/" resources under /resources/graphiql and
-    ; redirect "/graphiql" and "/graphiql/" -> "/graphiql/index.html"
-    {[::server/server ::syksy/syksy] {:port (or (System/getenv "PORT") 3000)}
-     [::resources/handler ::app] {:asset-prefix "/graphiql/"
-                                  :asset-dir "graphiql/"}
-     [::redirect/handler ::app] {:from (comp #{"/graphiql" "/graphiql/"} :uri)
-                                 :location "/graphiql/index.html"}}))
+  (let [port (or (some-> (System/getenv "PORT")
+                         (Integer/parseInt))
+                 3000)]
+    (p/deep-merge
+      (syksy/default-components {:index-body (index/index {:title "GraphQL API for TKL Journey API"})
+                                 :routes graphql/handler
+                                 :addon-handlers [(ig/ref [::resources/handler ::app])
+                                                  (ig/ref [::redirect/handler ::app])]})
+      ; Serve "/graphiql/" resources under /resources/graphiql and
+      ; redirect "/graphiql" and "/graphiql/" -> "/graphiql/index.html"
+      {[::server/server ::syksy/syksy] {:port port}
+       [::resources/handler ::app] {:asset-prefix "/graphiql/"
+                                    :asset-dir "graphiql/"}
+       [::redirect/handler ::app] {:from (comp #{"/graphiql" "/graphiql/"} :uri)
+                                   :location "/graphiql/index.html"}})))
