65f8601c7613b49f998add9463b6869d91b48ae3
Jarppe <jarppe@metosin.fi>
2018-04-09 15:40:50 +0300

    Use syksy 0.0.4

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
 
                  ; Syksy:
-                 [jarppe/syksy "0.0.3"]
+                 [jarppe/syksy "0.0.4"]
 
                  ; Common stuff:
                  [org.clojure/core.async "0.4.474"]
